3775dc72239a72143fc6187037f5e4db25fc7072
Shibhani13 <140389372+Shibhani13@users.noreply.github.com>
2025-08-16 16:19:34 +0530

    updates

---
+++ script.js
@@ -0,0 +1,71 @@
+// Form handling
+document.getElementById("contactForm").addEventListener("submit", e => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const msg = document.getElementById("message").value;
+  const result = document.getElementById("formResult");
+  // validation checks
+  if (!email) {
+    result.innerText = "Email cannot be empty.";
+    return;
+  }
+  if (!msg) {
+    result.innerText = "Password cannot be empty.";
+    return;
+  }
+  result.innerText =
+    `Submitted: ${email} - ${msg}`;
+});
+
+const slider = document.getElementById('volume');
+const output = document.getElementById('volume-value');
+
+slider.addEventListener('input', () => {
+  output.textContent = slider.value;
+});
+
+document.getElementById('start-btn').addEventListener('click', function () {
+  let progressBar = document.getElementById('progress-bar');
+  let progressText = document.getElementById('progress-text');
+  let width = 0;
+
+  let interval = setInterval(function () {
+    if (width >= 100) {
+      clearInterval(interval);
+    } else {
+      width++;
+      progressBar.style.width = width + '%';
+      progressText.textContent = width + '%';
+    }
+  }, 100);
+});
+
+// Open Shadow DOM
+class OpenShadow extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = `
+          <p>Inside <b>open</b> shadow DOM</p>
+          <button id="shadowBtn">Shadow Button</button>
+        `;
+    shadow.appendChild(wrapper);
+  }
+}
+customElements.define("open-shadow", OpenShadow);
+
+// Closed Shadow DOM
+class ClosedShadow extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "closed" });
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = `
+          <p>Inside <b>closed</b> shadow DOM</p>
+          <button id="secretBtn">Secret Button</button>
+        `;
+    shadow.appendChild(wrapper);
+  }
+}
+customElements.define("closed-shadow", ClosedShadow);
